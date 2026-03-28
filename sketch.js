cf90c78ddf44b4eb1ca1b7d30f7002201d7d9b4f
Dev7z <62627292+Dev7z@users.noreply.github.com>
2026-03-28 23:32:50 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,11 +6,11 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 const markers = [
-[[47.54134159140473, 7.549538764842142], "Bunker"]
-[[47.487583383740954, 7.625082130288086], "Villa (Torn down accourding to urbex-datenbank.ch)"]
-[[47.48371948257739, 7.4997036626881615], "Small factory building (unconfirmed, has cameras in the front entrance)"]
-[[47.531650, 7.679270], "Factory"]
-[[47.460390119754294, 7.871896547444343], "Farmhouse"]
+[[47.54134159140473, 7.549538764842142], "Bunker"],
+[[47.487583383740954, 7.625082130288086], "Villa (Torn down accourding to urbex-datenbank.ch)"],
+[[47.48371948257739, 7.4997036626881615], "Small factory building (unconfirmed, has cameras in the front entrance)"],
+[[47.531650, 7.679270], "Factory"],
+[[47.460390119754294, 7.871896547444343], "Farmhouse"],
 ];
 
 markers.forEach(addMarker);
