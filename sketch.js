bfe27b88e71a412bf524f51a9190874df6c679b7
Dev7z <62627292+Dev7z@users.noreply.github.com>
2026-03-28 22:49:33 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,11 +5,12 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker = L.marker([47.54594, 7.67773]).addTo(map);
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-
 const markers = [
-[[47.5, 7.75],"Saab"]
+[[47.54134159140473, 7.549538764842142], "Bunker"]
+[[47.487583383740954, 7.625082130288086], "Villa (Torn down accourding to urbex-datenbank.ch)"]
+[[47.48371948257739, 7.4997036626881615], "Small factory building (unconfirmed, has cameras in the front entrance)"]
+[[47.531650, 7.679270], "Factory"]
+[[47.460390119754294, 7.871896547444343], "Farmhouse"]
 ];
 
 markers.forEach(addMarker);
